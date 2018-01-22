2b5c74627330a187a71c6d6695419728aba022d0
Weskley Damasceno <weskleydamasceno@gmail.com>
2018-01-22 10:03:26 -0300

    Exemplo com Listas e Dicionários

--- weskley_dotnet/app/app/Program.cs
+++ weskley_dotnet/app/app/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using bll;
 
 namespace app
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Dictionary<string, int> pessoas = new Dictionary<string, int>();
+            pessoas.Add("pessoa1", 23);
+            pessoas.Add("pessoa2", 36);
+
+            foreach (KeyValuePair<string, int> pessoa in pessoas)
+            {
+                Console.WriteLine($"Nome: {pessoa.Key}");
+                Console.WriteLine($"Idade: {pessoa.Value}");
+            }
+
+
+            List<string> newPessoa = new List<string>();
+            newPessoa.Add("pessoa1");
+            newPessoa.Add("pessoa2");
+
+            Console.WriteLine($"Qtde de pessoas: {newPessoa.Count}");
         }
     }
 }
